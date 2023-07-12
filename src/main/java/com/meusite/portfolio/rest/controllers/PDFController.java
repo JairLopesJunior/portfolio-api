f21582e4999b9827eabe06c3572c997b949c4041
unknown <jair.lopes@fatec.sp.gov.br>
2023-07-11 21:26:02 -0300

    Criado service para gerar o arquivo .pfd

---
+++ src/main/java/com/meusite/portfolio/rest/controllers/PDFController.java
@@ -0,0 +1,33 @@
+package com.meusite.portfolio.rest.controllers;
+
+import com.itextpdf.io.source.ByteArrayOutputStream;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.meusite.portfolio.rest.services.PDFService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/pdf")
+public class PDFController {
+
+    @Autowired
+    private PDFService pdfService;
+
+    @CrossOrigin
+    @GetMapping
+    public ResponseEntity<Object> generateFile() {
+        return this.pdfService.generateFile();
+    }
+}
--- src/main/java/com/meusite/portfolio/rest/services/PDFService.java
+++ src/main/java/com/meusite/portfolio/rest/services/PDFService.java
@@ -1,41 +1,41 @@
-package com.meusite.portfolio.rest.controllers;
+package com.meusite.portfolio.rest.services;
 
+import com.itextpdf.io.font.FontConstants;
 import com.itextpdf.io.source.ByteArrayOutputStream;
+import com.itextpdf.kernel.colors.Color;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.property.TextAlignment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
-@CrossOrigin
-@RestController
-@RequestMapping("/pdf")
-public class PDFController {
+@Service
+public class PDFService {
 
     private static final String FILE_NAME = "curriculo.pdf";
 
     private static final String ATTACHMENT = "attachment";
 
-    @CrossOrigin
-    @GetMapping
+    private static final String JAIR_LOPES_JUNIOR = "Jair Lopes Junior";
+
+    private static final Integer VINTE = 20;
+
     public ResponseEntity<Object> generateFile() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
         try (PdfWriter writer = new PdfWriter(baos);
              PdfDocument pdfDocument = new PdfDocument(writer);
-             Document document = new Document(pdfDocument)) {
+             Document doc = new Document(pdfDocument)) {
 
-            document.add(new Paragraph("Exemplo de arquivo PDF gerado com iText 7"));
-            document.add(new Paragraph("Outra linha do documento PDF"));
-
+            doc.add(this.addParagraph(JAIR_LOPES_JUNIOR, VINTE, TextAlignment.CENTER));
         } catch (IOException e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().build();
@@ -48,5 +48,13 @@ public class PDFController {
         return ResponseEntity.ok()
                 .headers(headers)
                 .body(baos.toByteArray());
+    }
+
+    private Paragraph addParagraph(String text, Integer fontSize, TextAlignment textAlignment) {
+        Paragraph p = new Paragraph(text);
+        p.setFontSize(fontSize);
+        p.setTextAlignment(textAlignment);
+        p.setFontColor(Color.);
+        return p;
     }
 }
